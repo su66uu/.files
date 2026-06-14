7433e4a97d6c74ebe4c6990bc413d3ea4acb0534
Subbu <svenkat683@gmail.com>
2026-06-14 21:51:18 +0530

    Disable diagnostics for markdown files

--- nvim/.config/nvim/lua/custom/plugins/tiny-inline-diagnostics.lua
+++ nvim/.config/nvim/lua/custom/plugins/tiny-inline-diagnostics.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000, -- needs to be loaded in first
     config = function()
       require('tiny-inline-diagnostic').setup({
-        disabled_ft = { 'markdown' }, -- Disable for markdown files
+        disabled_ft = {}, -- Disable for markdown files
       })
       vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
     end,
