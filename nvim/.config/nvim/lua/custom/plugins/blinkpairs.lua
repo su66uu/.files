9f3523aed714506c721b675c6fa551f4a7e7a940
Subbu Myla <venkat.myla@MacBookPro.lan>
2026-07-15 18:50:31 +0530

    Use latest blink pairs config

--- nvim/.config/nvim/lua/custom/plugins/blinkpairs.lua
+++ nvim/.config/nvim/lua/custom/plugins/blinkpairs.lua
@@ -4,11 +4,14 @@ return {
     version = '*', -- (recommended) only required with prebuilt binaries
 
     -- download prebuilt binaries from github releases
-    dependencies = 'saghen/blink.download',
+    dependencies = 'saghen/blink.lib',
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
     -- build = 'nix run .#build-plugin',
+    build = function()
+      require('blink.pairs').download():pwait(60000)
+    end,
 
     opts = {
       mappings = {
