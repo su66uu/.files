ca55e31dc9089dd00d8692011cfcdba1060f77fa
Subbu <svenkat683@gmail.com>
2026-07-18 07:42:51 +0530

    Fix distracting behaviour with markdown plugin

--- nvim/.config/nvim/lua/custom/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/custom/plugins/markdown-preview.lua
@@ -1,5 +1,24 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim', 'echasnovski/mini.icons' },
-  opts = {},
+  dependencies = { 
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.nvim',
+    'echasnovski/mini.icons'
+  },
+  opts = {
+    -- Keep Markdown rendered in every mode, including Insert mode.
+    render_modes = true,
+
+    -- Do not reveal markdown beneath the cursor
+    anti_conceal = {
+      enabled = false,
+    },
+  },
+  keys = {
+    {
+      '<leader>mt',
+      '<cmd>RenderMarkdown buf_toggle<cr>',
+      desc = 'Toggle rendered/raw markdown'
+    }
+  }
 }
